c42bd08a849ae5dc5996348a9dec6427f0afd3b9
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2024-08-29 13:03:06 +0530

    stu.py

---
+++ stu.py
@@ -0,0 +1,56 @@
+Student_details = {
+    "John": {
+        "Age": 15,
+        "Class": 10,
+        "Roll no.": 2,
+        "Mother's Name": "Sheela",
+        "Father's Name": "Suresh",
+        "Address": "Chennai",
+        "Phone no.": 1234567890,
+        "Student ID": 11223344556677,
+    },
+    
+    "Jane": {
+        "Age": 15,
+        "Class": 9,
+        "Roll no.": 3,
+        "Mother's Name": "Sujatha",
+        "Father's Name": "Ramesh",
+        "Address": "Pune",
+        "Phone no.": 12344556909,
+        "Student ID": 11003344556677,
+    },
+    
+    "Jim": {
+        "Age": 17,
+        "Class": 11,
+        "Roll no.": 12,
+        "Mother's Name": "San",
+        "Father's Name": "David",
+        "Address": "Pune",
+        "Phone no.": 12344556909,
+        "Student ID": 11003311111677,
+    },
+    
+    "Jill": {
+        "Age": 17,
+        "Class": 10,
+        "Roll no.": 12,
+        "Mother's Name": "Sano",
+        "Father's Name": "Raiden",
+        "Address": "Pune",
+        "Phone no.": 12344556909,
+        "Student ID": 110033000667117,
+    },
+    
+    "Jack": {
+        "Age": 16,
+        "Class": 10,
+        "Roll no.": 12,
+        "Mother's Name": "Navita",
+        "Father's Name": "Adam",
+        "Address": "Pune",
+        "Phone no.": 12344556909,
+        "Student ID": 110033999999117,
+    },
+}
